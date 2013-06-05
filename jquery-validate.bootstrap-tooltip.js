6545dfc5a51128a64aa3d5bbd79be0ccac8ec799
wbp-tomh <tomh@webuildpages.com>
2013-06-05 02:23:52 -0400

    * Chaining javascript functions * Added missing check for existance of tooltip_options object * Adding minified version

--- jquery-validate.bootstrap-tooltip.js
+++ jquery-validate.bootstrap-tooltip.js
@@ -1,5 +1,5 @@
 /*!
- * jQuery Validation Bootstrap Tooltip extention v0.1
+ * jQuery Validation Bootstrap Tooltip extention v0.2
  *
  * https://github.com/wbp-tomh/jQuery-Validation-Bootstrap-tooltip
  *
@@ -7,40 +7,36 @@
  * Released under the MIT license:
  *   http://www.opensource.org/licenses/mit-license.php
  */
- 
 (function($) {
-
-$.extend(true,$.validator, {
-	prototype:{
-		defaultShowErrors: function() {
-			var self=this;
-			$.each( this.successList, function(index, value) {
-				$(value).tooltip('destroy');
-			});
-			$.each( this.errorList, function(index, value) {
-				$(value.element).tooltip('destroy');
-				$(value.element).tooltip(self.tooltip_options(value.element,value.message));
-				$(value.element).tooltip('show'); 
-			});
-		},
-		tooltip_options: function(element, message){
-			var options={
-				/* Using Twitter Bootstrap Defaults if no settings are given */ 
-				animation: $(element).data('animation')||true,
-				html: $(element).data('html')||false,
-				placement: $(element).data('placement')||'top',
-				selector: $(element).data('animation')||true,
-				title: $(element).attr('title')||message,
-				trigger: String('manual '+($(element).data('trigger')||'')).trim(),
-				delay: $(element).data('delay')||0,
-				container: $(element).data('container')||false
-			};
-			if(this.settings.tooltip_options[element.name]){
-				$.extend(options,this.settings.tooltip_options[element.name]);
-			}
-			return options;
-		},
-	}
-});
-
+	$.extend(true,$.validator, {
+		prototype:{
+			defaultShowErrors: function() {
+				var self=this;
+				$.each( this.successList, function(index, value) {
+					$(value).tooltip('destroy');
+				});
+				$.each( this.errorList, function(index, value) {
+					$(value.element).tooltip('destroy').tooltip(self.apply_tooltip_options(value.element,value.message)).tooltip('show'); 			
+				});
+			},
+			
+			apply_tooltip_options: function(element, message){
+				var options={
+					/* Using Twitter Bootstrap Defaults if no settings are given */ 
+					animation: $(element).data('animation')||true,
+					html: $(element).data('html')||false,
+					placement: $(element).data('placement')||'top',
+					selector: $(element).data('animation')||true,
+					title: $(element).attr('title')||message,
+					trigger: String('manual '+($(element).data('trigger')||'')).trim(),
+					delay: $(element).data('delay')||0,
+					container: $(element).data('container')||false
+				};
+				if(this.settings.tooltip_options&&this.settings.tooltip_options[element.name]){
+					$.extend(options,this.settings.tooltip_options[element.name]);
+				}
+				return options;
+			},
+		}
+	});
 }(jQuery));
