adee538e7e17eff68e25a54fb340d3fe4906b394
ThrillerAtPlay <thrilleratplay@gmail.com>
2015-09-29 01:18:07 -0400

    Version v0.10.0

--- js/jquery-validate.bootstrap-tooltip.js
+++ js/jquery-validate.bootstrap-tooltip.js
@@ -1,27 +1,51 @@
-/*!
- * jQuery Validation Bootstrap Tooltip extention v0.9.0
+/**
+ * @preserve
+ * jQuery Validation Bootstrap Tooltip extention v0.10.0
  *
  * https://github.com/Thrilleratplay/jQuery-Validation-Bootstrap-tooltip
  *
  * Copyright 2015 Tom Hiller
  * Released under the MIT license:
- *   http://www.opensource.org/licenses/mit-license.php
+ * http://www.opensource.org/licenses/mit-license.php
  */
 (function($) {
   $.extend(true, $.validator, {
     prototype: {
       defaultShowErrors: function() {
         var _this = this;
+        var bsVersion = $.fn.tooltip.Constructor.VERSION;
+        var bsMajorVer =  0;
+        var bsMinorVer = 0;
+
+        // Try to determine Bootstrap major and minor versions
+        if (bsVersion) {
+          bsVersion = bsVersion.split('.');
+          bsMajorVer = parseInt(bsVersion[0]);
+          bsMinorVer = parseInt(bsVersion[1]);
+        }
 
         $.each(this.errorList, function(index, value) {
-          $(value.element).removeClass(_this.settings.validClass).addClass(_this.settings.errorClass).tooltip('destroy').tooltip(_this.applyTooltipOptions(value.element, value.message)).tooltip('show');
+          //If Bootstrap 3.3 or greater
+          if (bsMajorVer === 3 && bsMinorVer >= 3) {
+            var currentElement = $(value.element);
+            if (currentElement.data('bs.tooltip') !== undefined) {
+              currentElement.data('bs.tooltip').options.title = value.message;
+            } else {
+              currentElement.tooltip(_this.applyTooltipOptions(value.element, value.message));
+            }
+
+            $(value.element).removeClass(_this.settings.validClass).addClass(_this.settings.errorClass).tooltip('show');
+          } else {
+            $(value.element).removeClass(_this.settings.validClass).addClass(_this.settings.errorClass).tooltip('destroy').tooltip(_this.applyTooltipOptions(value.element, value.message)).tooltip('show');
+          }
+
           if (_this.settings.highlight) {
             _this.settings.highlight.call(_this, value.element, _this.settings.errorClass, _this.settings.validClass);
           }
         });
 
         $.each(_this.validElements(), function(index, value) {
-          $(value).removeClass(_this.settings.errorClass).addClass(_this.settings.validClass).tooltip('destroy');
+          $(value).removeClass(_this.settings.errorClass).addClass(_this.settings.validClass).tooltip(bsMajorVer === 4 ? 'dispose' : 'destroy');
           if (_this.settings.unhighlight) {
             _this.settings.unhighlight.call(_this, value, _this.settings.errorClass, _this.settings.validClass);
           }
@@ -38,7 +62,7 @@
           title: $(element).attr('title') || message,
           trigger: $.trim('manual ' + ($(element).data('trigger') || '')),
           delay: $(element).data('delay') || 0,
-          container: $(element).data('container') || false
+          container: $(element).data('container') || false,
         };
 
         if (this.settings.tooltip_options && this.settings.tooltip_options[element.name]) {
@@ -50,7 +74,7 @@
         }
         /* jshint ignore:end */
         return options;
-      }
-    }
+      },
+    },
   });
 }(jQuery));
